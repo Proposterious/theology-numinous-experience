324a50a1e3c765842fd9b1eaf5a3447fe3b2fbda
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-08 20:44:31 -0500

    Create sources page

---
+++ numinous-experience/app/sources/page.tsx
@@ -0,0 +1,13 @@
+function Sources() {
+    return ( 
+        <article>
+            <section>
+                <h1>
+                    Sources
+                </h1>
+            </section>
+        </article>
+     );
+}
+
+export default Sources;
