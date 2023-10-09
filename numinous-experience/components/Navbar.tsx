2beeffe449936b012a5a87ba238887a5a7e129a4
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-09 15:20:13 -0500

    Edit hover behavior of list

--- numinous-experience/components/Navbar.tsx
+++ numinous-experience/components/Navbar.tsx
@@ -4,22 +4,22 @@ function Navbar() {
     return ( 
         <nav id="primary-nav" className="w-full | bg-pink-200/50 py-6"> 
             <ul className="w-full justify-center list-none flex flex-row space-x-10 | font-semibold text-xl text-teal-400">
-                <li className="hover:text-green-400" id="home">
+                <li className="hover:text-green-700" id="home">
                     <Link href="/">
                         Home
                     </Link>
                 </li>
-                <li className="hover:text-green-400" id="about">
+                <li className="hover:text-green-700" id="about">
                     <Link href="/about">
                         About
                     </Link>
                 </li>
-                <li className="hover:text-green-400" id="contact">
+                <li className="hover:text-green-700" id="contact">
                     <Link href="/contact">
                         Contact
                     </Link>
                 </li>
-                <li className="hover:text-green-400" id="sources">
+                <li className="hover:text-green-700" id="sources">
                     <Link href="/sources">
                         Sources
                     </Link>
