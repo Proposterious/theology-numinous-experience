f894136f82743a138121965f0c040299c2194d49
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-09 15:20:43 -0500

    Add content to 'background', Edit styles

--- numinous-experience/app/page.tsx
+++ numinous-experience/app/page.tsx
@@ -1,12 +1,14 @@
 function Home() {
   return ( 
     <main id="home" className="h-full">
-        <article id="opener" className=" w-full flex py-52 | bg-center bg-cover bg-[url('../public/church_sermon.webp')]">
-          <section className='mx-auto w-full py-24 bg-gradient-to-l from-purple-500 to-pink-500'>
+        <article id="opener" className=" w-full flex py-48 | bg-center bg-cover bg-[url('../public/church_sermon.webp')]">
+          <section className='mx-auto w-full py-16 bg-gradient-to-l from-purple-500 to-pink-500'>
             <h1 className='text-6xl text-center font-semibold text-teal-300 mb-12'>
-              Want or Don&apos;t Want
-              <br></br>
-              <span className="">a Numinous Experience?</span>
+              <span className="font-bold">Want</span> or <span className="font-bold">Don&apos;t Want</span>
+              <br />
+              to Have a Grand
+              <br />
+              <span className="font-bold italic">Numinous Experience?</span>
             </h1>
             <p className="font-semibold text-center text-2xl text-white">
               Follow Our Guide on How to
@@ -17,30 +19,33 @@ function Home() {
         </article>
 
       <article id="background" className="bg-slate-200 | border-t-4 border-t-emerald-400">
-        <section className="px-8 py-16 justify-evenly | border-orange-600 | flex flex-row">
-          <div className='w-1/4'>
-            <h1 className='-indent-2 text-4xl leading-tight font-bold text-teal-400 mb-4 w-fit mx-auto'>
+        <section className="px-8 pb-12 justify-evenly | border-orange-600 | flex flex-row">
+          <div id="wholly" className='w-1/3 mt-12 py-2 border-r-4 border-r-teal-400'>
+            <h1 className='-indent-2 text-4xl leading-tight font-bold text-teal-400 mb-2 w-fit mx-auto'>
               &quot;Wholly Other&quot;
             </h1>
-            <p className='w-3/4 text-center text-lg text-teal-800 mx-auto'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi reprehenderit suscipit quo at incidunt dicta omnis aliquid quasi rem ipsam?
-            </p>
+            <div className='w-3/4 text-center text-lg text-teal-900/80 mx-auto'>
+              <p className="text-slate-700 border-b-4 border-b-teal-400 mb-4">Derived from Latin&apos;s <i>&quot;numen&quot;</i> and <i>&quot;omen&quot;</i> (&apos;ominous&apos;)</p>
+              A variable that remains undefined by the population which cannot be taught nor understood by solely by its definition; only by its evocation and awakening may it manifest as a somewhat omnipresent, never-ending &quot;X&quot; &apos;of the spirit&apos; (<b>Rudolf O.</b>).
+            </div>
           </div>
-          <div className='w-1/4'>
-            <h1 className='text-4xl leading-tight font-bold text-teal-400 mb-4 w-fit mx-auto'>
+          <div id="mysterium" className='w-1/3 py-2 mt-12'>
+            <h1 className='text-4xl leading-tight font-bold text-teal-400 mb-2 w-fit mx-auto'>
               &quot;Mysterium&quot;
               </h1>
-            <p className='w-3/4 text-center text-lg text-teal-800 mx-auto'>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad natus molestiae dicta nulla cum. Nisi dolor eveniet minus quas autem.
-            </p>
+            <div className='w-3/4 text-center text-lg text-teal-900/80 mx-auto'>
+              <p className="text-slate-700 border-b-4 border-b-teal-400 mb-4">Derived from Latin&apos;s <i>&quot;numen&quot;</i> and <i>&quot;omen&quot;</i> (&apos;ominous&apos;)</p>
+              A variable that remains undefined by the population which cannot be taught nor understood by solely by its definition; only by its evocation and awakening may it manifest as a somewhat omnipresent, never-ending &quot;X&quot; &apos;of the spirit&apos; (<b>Rudolf O.</b>).
+            </div>
           </div>
-          <div className='w-1/4'>
-            <h1 className='-indent-2 text-4xl leading-tight font-bold text-teal-400 mb-4 w-fit mx-auto'>
-              &quot;Fascinan&quot;
+          <div id="numinous" className='w-1/3 mt-12 py-2 border-l-4 border-l-teal-400'>
+            <h1 className='-indent-2 text-4xl leading-tight font-bold text-teal-400 mb-2 w-fit mx-auto'>
+              &quot;Numinous&quot;
               </h1>
-            <p className='w-3/4 text-center text-lg text-teal-800 mx-auto'>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, repudiandae fugit temporibus sit reiciendis autem distinctio. Similique totam placeat quam.
-            </p>
+            <div className='w-3/4 text-center text-lg text-teal-900/80 mx-auto'>
+              <p className="text-slate-700 border-b-4 border-b-teal-400 mb-4">Derived from Latin&apos;s <i>&quot;numen&quot;</i> and <i>&quot;omen&quot;</i> (&apos;ominous&apos;)</p>
+              Not to be confused with the rationalized concept &apos;holy&apos; of the modern world. A thing that is <i>numinous</i> is variable that remains undefined by the population which cannot be taught nor understood by solely by its definition; only by its evocation and awakening may it emerge as a somewhat omnipresent, forever lasting manifestation of the divine &apos;spirit&apos; (<b>Rudolf O.</b>)
+            </div>
           </div>
         </section>
       </article>
