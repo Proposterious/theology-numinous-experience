3307c161ce733037a7f948277db33f2f8a195f53
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-08 20:43:52 -0500

    Create about page

---
+++ numinous-experience/app/about/page.tsx
@@ -0,0 +1,13 @@
+function About() {
+    return ( 
+        <article>
+            <section>
+                <h1>
+                    About this Website
+                </h1>
+            </section>
+        </article>
+     );
+}
+
+export default About;
