a8f208f86ca8d5738704a3843070b2718c6ded4b
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-08 20:43:29 -0500

    Add Footer to layout

--- numinous-experience/app/layout.tsx
+++ numinous-experience/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 // Custom imports
 import Navbar from "@/components/Navbar";
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
