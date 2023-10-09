192d981df6fa168175c5f33332fee1dc9e60187f
Proposterious <98131932+Proposterious@users.noreply.github.com>
2023-10-08 20:45:09 -0500

    Update Navbar

--- numinous-experience/components/Navbar.tsx
+++ numinous-experience/components/Navbar.tsx
@@ -1,18 +1,28 @@
+import Link from "next/link";
+
 function Navbar() {
     return ( 
-        <nav id="primary-nav" className="w-full | bg-teal-300 py-4"> 
-            <ul className="w-full justify-center list-none flex flex-row space-x-4 | font-semibold text-lg text-black">
-                <li id="home">
-                    Home
+        <nav id="primary-nav" className="w-full | bg-pink-200/50 py-6"> 
+            <ul className="w-full justify-center list-none flex flex-row space-x-10 | font-semibold text-xl text-teal-400">
+                <li className="hover:text-green-400" id="home">
+                    <Link href="/">
+                        Home
+                    </Link>
                 </li>
-                <li id="about">
-                    About
+                <li className="hover:text-green-400" id="about">
+                    <Link href="/about">
+                        About
+                    </Link>
                 </li>
-                <li id="contact">
-                    Contact
+                <li className="hover:text-green-400" id="contact">
+                    <Link href="/contact">
+                        Contact
+                    </Link>
                 </li>
-                <li id="sources">
-                    Sources
+                <li className="hover:text-green-400" id="sources">
+                    <Link href="/sources">
+                        Sources
+                    </Link>
                 </li>
             </ul>
         </nav>
